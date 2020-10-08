239658ad61af97d22beb376554fac82f8b08c92d
yitianj <55176584+yitianj@users.noreply.github.com>
2020-10-08 12:26:25 -0500

    Update singleimg.py

--- src/singleimg.py
+++ src/singleimg.py
@@ -28,16 +28,16 @@ for i in range(height):
 gray_img[i,j] = np.uint8(gray)
 median_img = cv.medianBlur(gray_img,9)
 ret,thresh = cv.threshold(median_img,120,255,cv.THRESH_BINARY)
-kernel = np.ones((5,5), np.uint8)#设置开闭运算卷积核
+kernel = np.ones((5,5), np.uint8)
 open_result = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
 close_result = cv.morphologyEx(open_result, cv.MORPH_CLOSE, kernel)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-x, y, w, h = cv.boundingRect(contours[1]) # 找到边界坐标
-#cv.rectangle(close_result, (x, y), (x+w, y+h), (0, 255, 0), 2)# 计算点集最外面的矩形边界
-rect = cv.minAreaRect(contours[1])# 找面积最小的矩形
-box = cv.boxPoints(rect) # 得到最小矩形的坐标
-box = np.int0(box)# 标准化坐标到整数
-#cv.drawContours(close_result, [box], 0, (0, 0, 255), 3) # 画出边界
+x, y, w, h = cv.boundingRect(contours[1]) 
+#cv.rectangle(close_result, (x, y), (x+w, y+h), (0, 255, 0), 2)
+rect = cv.minAreaRect(contours[1])
+box = cv.boxPoints(rect) 
+box = np.int0(box)
+
 
 cut_img = imagecrop(close_result,box)
 cv.imwrite("contour_cut_img.bmp",cut_img)
